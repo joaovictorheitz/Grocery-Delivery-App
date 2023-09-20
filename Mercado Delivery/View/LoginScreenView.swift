5fe8a53d3d049dc3631abb433712b46440b5c1b7
joaovictorheitz <vcitorsguedes@hotmail.com>
2023-09-20 16:07:17 -0300

    feat: SplashScreen

---
+++ Mercado Delivery/View/LoginScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreenView.swift
+//  Mercado Delivery
+//
+//  Created by João Victor de Souza Guedes on 20/09/23.
+//
+
+import SwiftUI
+
+struct LoginScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreenView()
+    }
+}
---
+++ Mercado Delivery/View/SplashScreenView.swift
@@ -0,0 +1,33 @@
+//
+//  SplashScreenView.swift
+//  Mercado Delivery
+//
+//  Created by João Victor de Souza Guedes on 20/09/23.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            ZStack {
+                Image("SplashScreen")
+            }
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+                    self.isActive = true
+                })
+            }
+        }
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
