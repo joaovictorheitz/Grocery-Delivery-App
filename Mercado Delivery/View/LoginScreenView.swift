dccfd939cd0bb1ec2fdd9dc073e8535a69b28758
joaovictorheitz <vcitorsguedes@hotmail.com>
2023-09-20 17:22:48 -0300

    Feat: Login Inputs

--- Mercado Delivery/View/LoginScreenView.swift
+++ Mercado Delivery/View/LoginScreenView.swift
@@ -7,9 +7,50 @@
 
 import SwiftUI
 
+
+
+
 struct LoginScreenView: View {
+    public init(email: String = "", password: String = "") {
+        self.email = email
+        self.password = password
+    }
+    
+    @State private var email: String
+    @State private var password: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 252/255, green: 252/255, blue: 252/255)
+            
+            VStack {
+                Image("LoginBanner")
+                    .frame(width: 413.37, height: 290.0)
+
+                Spacer()
+            }
+            
+            HStack {
+                Text("Login")
+                    .font(.system(size:40, weight: .bold))
+                
+                Spacer()
+            }
+            .offset(x: 40, y: -40)
+            
+            VStack(alignment: .trailing) {
+                TextField1(label: "Email", value: $email, security: false)
+                    .padding(.bottom)
+                TextField1(label: "Senha", value: $password, security: true)
+                
+                Text("Esqueceu sua senha?")
+                    .foregroundColor(Color(red: 82/255, green: 204/255, blue: 109/255))
+                    
+            }
+            .padding(.top, 120.0)
+            
+            Spacer()
+        }.edgesIgnoringSafeArea(.all)
     }
 }
 
---
+++ Mercado Delivery/View/TextField1.swift
@@ -0,0 +1,40 @@
+//
+//  InputView.swift
+//  Mercado Delivery
+//
+//  Created by João Victor de Souza Guedes on 20/09/23.
+//
+
+import SwiftUI
+
+struct TextField1: View {
+    public var label: String
+    public var value: Binding<String>
+    public var security: Bool
+    
+    var body: some View {
+        VStack {
+            if(security) {
+                SecureField(label, text: value)
+                    .frame(width: 300, height: 25)
+                Rectangle()
+                    .frame(width: 300, height: 1.0)
+                    .opacity(0.5)
+            } else {
+                TextField(label, text: value)
+                    .frame(width: 300.0, height: 25)
+                Rectangle()
+                    .frame(width: 300.0, height: 1.0)
+                    .opacity(0.5)
+            }
+        }
+    }
+}
+
+struct TextField1_Previews: PreviewProvider {
+    @State static private var x: String = ""
+    
+    static var previews: some View {
+        TextField1(label: "Email", value: $x, security: true)
+    }
+}
