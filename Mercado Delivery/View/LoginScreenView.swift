dd9978c01a1f3ef65f11b5d5fd9cecf2d64a3968
joaovictorheitz <vcitorsguedes@hotmail.com>
2023-09-20 17:39:51 -0300

    Feat: Login Button

--- Mercado Delivery/View/LoginScreenView.swift
+++ Mercado Delivery/View/LoginScreenView.swift
@@ -24,32 +24,45 @@ struct LoginScreenView: View {
             Color(red: 252/255, green: 252/255, blue: 252/255)
             
             VStack {
-                Image("LoginBanner")
-                    .frame(width: 413.37, height: 290.0)
+                VStack {
+                    Image("LoginBanner")
+                        .frame(width: 413.37, height: 290.0)
 
-                Spacer()
-            }
-            
-            HStack {
-                Text("Login")
-                    .font(.system(size:40, weight: .bold))
+                    Spacer()
+                }
+                .frame(height: 290)
+                //
                 
-                Spacer()
-            }
-            .offset(x: 40, y: -40)
-            
-            VStack(alignment: .trailing) {
-                TextField1(label: "Email", value: $email, security: false)
-                    .padding(.bottom)
-                TextField1(label: "Senha", value: $password, security: true)
-                
-                Text("Esqueceu sua senha?")
-                    .foregroundColor(Color(red: 82/255, green: 204/255, blue: 109/255))
+                HStack {
+                    Text("Login")
+                        .font(.system(size:40, weight: .bold))
                     
+                    Spacer()
+                }
+                .offset(x: 40, y: -20)
+                
+                VStack(alignment: .trailing) {
+                    TextField1(label: "Email", value: $email, security: false)
+                        .padding(.bottom)
+                    TextField1(label: "Senha", value: $password, security: true)
+                    
+                    Text("Esqueceu sua senha?")
+                        .foregroundColor(Color(red: 82/255, green: 204/255, blue: 109/255))
+                        .padding(.top, 2.0)
+                }
+                
+                HStack {
+                    Button("Entrar") {
+                        
+                    }
+                    .padding(12)
+                    .frame(width:300)
+                    .background(Color(red: 82/255, green: 204/255, blue: 109/255))
+                    .cornerRadius(10)
+                }
+                
+                Spacer()
             }
-            .padding(.top, 120.0)
-            
-            Spacer()
         }.edgesIgnoringSafeArea(.all)
     }
 }
