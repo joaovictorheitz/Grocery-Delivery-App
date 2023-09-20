a6b67ad20f6bb3e363c7b7d6f5c6970c1d025dfe
joaovictorheitz <vcitorsguedes@hotmail.com>
2023-09-20 15:39:40 -0300

    Initial Commit

---
+++ Mercado Delivery/Mercado_DeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mercado_DeliveryApp.swift
+//  Mercado Delivery
+//
+//  Created by João Victor de Souza Guedes on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Mercado_DeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
